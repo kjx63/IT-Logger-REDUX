01abc0619e9733c31ab93e578dfcd68a0f348252
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-06 09:47:46 +0900

    Connecting Redux To a Component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
   }, []);
 
-  const getLogs = () => {
-    setLoading(true);
-
-    setTimeout(async () => {
-      const res = await fetch('/logs');
-      const data = await res.json();
-
-      setLogs(data);
-      setLoading(false);
-    }, 1200);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
   return (
@@ -43,4 +31,12 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
